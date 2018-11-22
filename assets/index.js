0d7cfd47c40b8816b1d5d16350157dc519ccb5c1
Dylan Kok <endylankjy@gmail.com>
2018-11-22 22:57:10 +0800

    Update

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,12 @@
 /* Copyright (c) Dylan Kok (Koku Shunage) 2018. Distribution and unauthorised storage of this file is prohibited */
 console.log('Okay, Loaded. By Dylan Kok (Koku Shunage)')
 
-/* Detect when to timeout session */
+// Detect when to timeout session
 let timeout;
 document.onmousemove = function () {
     if ($("#welcome").is(":hidden") === true) {
         clearTimeout(timeout);
-        timeout = setTimeout(function () { window.location.reload(false) }, 60000);
+        timeout = setTimeout(function () { afk(); }, 60000);
     }
 }
 
@@ -14,19 +14,46 @@ function sessionReset() {
     window.location.reload(false)
 }
 
-/* Code hints for Try it */
-let hint_tryit1 = `# Start with typing your name. Maybe start a variable called 'name'
-# Hint: name = ____
-name = "(enter your name here)"
+// Warn AFK
+let afkModalTimer
+function afk() {
+    console.log("[DEBUG] afk()")
+    $("#afk-warn").addClass("is-active");
+    afkModalTimer = setTimeout(function () { sessionReset() }, 10000);
+}
 
-# Now, you can try printing. Hint: Use two 'print'. One for 'Hello ...' & other for your name
-print('')
-print()
+function afkActive() {
+    clearTimeout(afkModalTimer)
+    $("#afk-warn").removeClass("is-active");
+}
 
-# Try to display 'Hello my name is 
-#(your name)'
-# For example, 'Hello my name is 
-#Coconut'`
+// Code hints for Try it
+let hint_tryit1 = `# Use print to print your name
+# Here's an example, print('Shigami')
+
+`
+let hint_tryit2 = `# Start with variables. You may use any number.
+x = 
+y = 
+
+# Add x and y and assign it to a variable called, result
+result =
+
+# Print result. This has already been done for you.
+print(result)`
+
+let hint_tryit3 = `# Start with variables. 
+# You may input any marks to test your code.
+marks = 
+
+# Check if marks are higher than 75. Example: if a <= 10:
+# Make an if statement below this line
+
+[[(if statement - replace this line with your if statement)]]
+    print("This student has gotten an 'A' grade.")
+else:
+    print("This mark is not an 'A'")
+`
 
 function page(switchInput) {
     switch (switchInput) {
@@ -51,22 +78,120 @@ function page(switchInput) {
         case 'TRYIT-1': {
             /*$(".page-footer").removeClass( "pos-absolute" ).addClass( "pos-relative" );*/
             $('#lesson2').hide()
-            $('#lesson3').hide()
+            $('#tryit2').hide()
             $('#code-section').show()
             $('#tryit1').show()
             /* Setup hints in code sec */
             $('#yourcode').val(hint_tryit1);
             break;
         }
+        case 'TRYIT-2': {
+            /*$(".page-footer").removeClass( "pos-absolute" ).addClass( "pos-relative" );*/
+            $('#tryit1').hide()
+            $('#lesson3').hide()
+            $('#code-section').show()
+            $('#tryit2').show()
+            /* Setup hints in code sec */
+            $('#yourcode').val(hint_tryit2);
+            break;
+        }
         case 'LESSON-3': {
             /*$(".page-footer").removeClass( "pos-relative" ).addClass( "pos-absolute" );*/
-            $('#tryit1').hide()
+            $('#tryit2').hide()
+            $('#tryit3').hide()
             $('#code-section').hide()
             $('#lesson3').show()
             break;
         }
+        case 'TRYIT-3': {
+            /*$(".page-footer").removeClass( "pos-relative" ).addClass( "pos-absolute" );*/
+            $('#lesson3').hide()
+            $('#code-section').show()
+            $('#yourcode').val(hint_tryit3);
+            $('#tryit3').show()
+            break;
+        }
+        case 'END': {
+            /*$(".page-footer").removeClass( "pos-relative" ).addClass( "pos-absolute" );*/
+            $('#tryit3').hide()
+            $('#code-section').hide()
+            $('#resetSession').hide()
+            $('#end').show()
+            setTimeout(function () { window.location.reload(false) }, 5000)
+            break;
+        }
         default: {
             console.log("Hmmmm... Something went wrong. I'll refresh the page to fix it.")
+            alert("Hmmmm... Something went wrong. I'll refresh the page to fix it.")
+            window.location.reload(true)
         }
     }
+}
+
+// Clear console output
+function clearConsole() {
+    var ta = document.getElementById('output'); {
+        ta.value = '';
+    }
+}
+
+// Allow tabs in Text Areas
+function insertTab(o, e) {
+    var kC = e.keyCode ? e.keyCode : e.charCode ? e.charCode : e.which;
+    if (kC == 9 && !e.shiftKey && !e.ctrlKey && !e.altKey) {
+        var oS = o.scrollTop;
+        if (o.setSelectionRange) {
+            var sS = o.selectionStart;
+            var sE = o.selectionEnd;
+            o.value = o.value.substring(0, sS) + "\t" + o.value.substr(sE);
+            o.setSelectionRange(sS + 1, sS + 1);
+            o.focus();
+        }
+        else if (o.createTextRange) {
+            document.selection.createRange().text = "\t";
+            e.returnValue = false;
+        }
+        o.scrollTop = oS;
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+        return false;
+    }
+    return true;
+}
+
+// Skrupt
+// output functions are configurable.  This one just appends some text
+// to a pre element.
+function outf(text) {
+    var mypre = document.getElementById("output");
+    mypre.innerHTML = mypre.innerHTML + text;
+}
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+        throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+// Here's everything you need to run a python program in skulpt
+// grab the code from your textarea
+// get a reference to your pre element for output
+// configure the output function
+// call Sk.importMainWithBody()
+function runit() {
+    var prog = document.getElementById("yourcode").value;
+    var mypre = document.getElementById("output");
+    mypre.innerHTML = '';
+    Sk.pre = "output";
+    Sk.configure({ output: outf, read: builtinRead });
+    var myPromise = Sk.misceval.asyncToPromise(function () {
+        return Sk.importMainWithBody("<stdin>", false, prog, true);
+    });
+    myPromise.then(function (mod) {
+        console.log('success');
+    },
+        function (err) {
+            console.log(err.toString());
+            alert("ERROR OUTPUT:     " + err);
+        });
 }
---
+++ solutions/tryit-1.py
@@ -0,0 +1,11 @@
+# Start with variables. 
+# You may input any marks to test your code.
+marks = (input any number here)
+
+# Check if marks are higher than 75. Example: if a <= 10:
+# Make an if statement below this line
+
+if marks <= 75:
+    print("This student has gotten an 'A' grade.")
+else:
+    print("This mark is not an 'A'")
